f87353dba1429baa8a00b3ec9eb59e300f4cf989
danipatelle <109215494+danipatelle@users.noreply.github.com>
2023-01-19 15:26:03 +0000

    checkboxes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,23 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/page2-answer', function (req, res) {
+    var start = req.session.data['do-you-want-to-start'] 
+    if(start=='Yes'){   
+        res.redirect('continue')
+    }
+    if(start=='No'){   
+        res.redirect('cancel')
+    }
+})
+
+router.post('/checkboxes-answer', function (req, res) {
+    var checkbox = req.session.data['which-of-the-following-are-true'] 
+    if(checkbox=='I am over 18'){   
+        console.log('hello')
+        res.redirect('nextpage')
+    }
+    if(checkbox=='I am a woman','I am married'){   
+        res.redirect('unsuccessful')
+    }
+})
